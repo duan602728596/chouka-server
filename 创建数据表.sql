a58b1e80ad4b1c27f88280ecdbf5d79055758668
duanhaochen <duanhaochen@126.com>
2018-03-25 19:04:08 +0800

    添加创建数据表的sql脚本

---
+++ 创建数据表.sql
@@ -0,0 +1,26 @@
+-- --------------------------------------------------------
+-- 主机:                           localhost
+-- 服务器版本:                        10.2.13-MariaDB - mariadb.org binary distribution
+-- 服务器操作系统:                      Win64
+-- HeidiSQL 版本:                  9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- 导出  表 huangtongyang.chouka1 结构
+CREATE TABLE IF NOT EXISTS `chouka1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` varchar(50) NOT NULL COMMENT '用户id',
+  `nickname` varchar(50) NOT NULL COMMENT '用户昵称',
+  `record` text NOT NULL COMMENT '卡的数量，json格式',
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='抽卡1';
+
+-- 数据导出被取消选择。
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
