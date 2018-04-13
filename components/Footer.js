9f6d536a765adaf11b1dffcc14678dbebfa7db1c
duanhaochen <duanhaochen@126.com>
2018-04-13 12:55:56 +0800

    调整备案信息

--- components/Footer.js
+++ components/Footer.js
@@ -6,8 +6,11 @@ class Footer extends Component{
     return (
       <footer className="text-center footer">
         <span>黄彤扬应援网</span>
-        {/* <a className="footer-ml30" href="#">京公网安备 11111111111111</a> */}
-        {/* <a className="footer-ml10" href="#">京ICP备11111111号</a> */}
+        <a className="footer-ml30" href="http://www.miitbeian.gov.cn" target="_blank">京ICP备11111111号</a>
+        <a className="footer-ml10" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=" target="_blank">
+          <img className="footer-beian" src="/static/beian.png" />
+          京公网安备 11111111111111
+        </a>
       </footer>
     );
   }
